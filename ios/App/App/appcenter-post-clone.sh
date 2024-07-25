647338b73e6c56cbec46b0ec44b970fdb715d437
Jnpvl <151414138+Jnpvl@users.noreply.github.com>
2024-07-25 15:53:17 -0700

    hola

--- ios/App/App/appcenter-post-clone.sh
+++ ios/App/App/appcenter-post-clone.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Uninstalling all CocoaPods versions"
+# Imprime un mensaje para indicar el inicio del script
+echo "Iniciando el script appcenter-post-clone.sh"
+
+# Desinstala todas las versiones de CocoaPods
+echo "Desinstalando todas las versiones de CocoaPods"
 sudo gem uninstall cocoapods --all --executables
 
-COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
+# Imprime un mensaje después de desinstalar
+echo "CocoaPods desinstalado. Continuando con la instalación..."
 
+# Extrae la versión de CocoaPods desde el archivo Podfile.lock
+COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: $begin:math:text$[0-9.]*$end:math:text$/\1/p' Podfile.lock`
+echo "Versión extraída de CocoaPods desde Podfile.lock: $COCOAPODS_VER"
+
+# Instala CocoaPods
+echo "Instalando CocoaPods..."
 sudo gem install cocoapods
-echo "cocoapodsintstalado"
-echo cocoapods -v
-echo "hasta aca llegamos"
+
+# Imprime un mensaje después de la instalación
+echo "CocoaPods instalado. Versión actual:"
+cocoapods -v
+
+# Imprime un mensaje para indicar que el script ha terminado
+echo "Fin del script appcenter-post-clone.sh"
