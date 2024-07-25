68a49788f5d0c055980efbe3fb50c16ff4fa19fd
Jnpvl <151414138+Jnpvl@users.noreply.github.com>
2024-07-25 15:50:56 -0700

    dasok

--- ios/App/App/appcenter-post-clone.sh
+++ ios/App/App/appcenter-post-clone.sh
@@ -5,10 +5,7 @@ sudo gem uninstall cocoapods --all --executables
 
 COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
 
-echo "Installing CocoaPods version $COCOAPODS_VER"
-sudo gem install cocoapods -v $COCOAPODS_VER
-
-console.log('instalar los siguiente')
-sudo arch -x86_64 gem install ffi
-arch -x86_64 pod install
-sudo -x86_64 pod install
+sudo gem install cocoapods
+echo "cocoapodsintstalado"
+echo cocoapods -v
+echo "hasta aca llegamos"
