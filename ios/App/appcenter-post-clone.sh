c8800242473d0fd3bc305ff41373f9d455de6c50
Jnpvl <151414138+Jnpvl@users.noreply.github.com>
2024-07-25 17:03:13 -0700

    da

--- ios/App/appcenter-post-clone.sh
+++ ios/App/appcenter-post-clone.sh
@@ -2,26 +2,29 @@
 
 echo "Iniciando el script appcenter-post-clone.sh"
 echo "donde estoy"
-pwd  #/Users/runner/work/1/s/ios/App
+pwd  # Esto mostrará: /Users/runner/work/1/s/ios/App
+
 echo "aca voy"
-cd cd ../../..
-pwd
+cd ../../..  # Cambiamos tres niveles hacia arriba.
+pwd  # Esto mostrará: /Users/runner/work/1/s
+
 # Instala las dependencias de Node
 echo "Instalando dependencias de Node..."
-npm install
+npm install  # Este comando se ejecutará en: /Users/runner/work/1/s
+
 # Verifica que las dependencias de Node estén instaladas
-pwd
+pwd  # Este comando mostrará: /Users/runner/work/1/s
 echo "Verificando que node_modules contiene @capacitor/ios..."
-ls -l ../node_modules/@capacitor/ios/scripts/pods_helpers.rb
+ls -l ../node_modules/@capacitor/ios/scripts/pods_helpers.rb  # Esto verificará la existencia del archivo en: /Users/runner/work/1/node_modules/@capacitor/ios/scripts/pods_helpers.rb
 
 # Desinstala todas las versiones de CocoaPods
 echo "Desinstalando todas las versiones de CocoaPods"
-sudo gem uninstall cocoapods --all --executables
+sudo gem uninstall cocoapods --all --executables  # Este comando se ejecuta en: /Users/runner/work/1/s
 
 echo "CocoaPods desinstalado. Continuando con la instalación..."
 
 # Extrae la versión de CocoaPods desde el archivo Podfile.lock
-COCOAPODS_VER=$(sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock)
+COCOAPODS_VER=$(sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock)  # Este comando se ejecuta en: /Users/runner/work/1/s
 if [ -z "$COCOAPODS_VER" ]; then
     echo "No se pudo extraer la versión de CocoaPods desde Podfile.lock"
 else
@@ -30,11 +33,11 @@ fi
 
 # Instala CocoaPods
 echo "Instalando CocoaPods..."
-sudo gem install cocoapods
+sudo gem install cocoapods  # Este comando se ejecuta en: /Users/runner/work/1/s
 
 # Asegúrate de que el comando `pod` está en el PATH
 echo "Verificando instalación de CocoaPods..."
-which pod
+which pod  # Este comando se ejecuta en: /Users/runner/work/1/s
 if [ $? -eq 0 ]; then
     echo "CocoaPods instalado correctamente. Versión actual:"
     pod --version
